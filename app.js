36eb2ea77df0e7d5c27f1997eaaef33abf0d9d8a
tiki-stack <tyrik.nelson1@gmail.com>
2021-06-01 17:01:30 -0400

    complete project

--- app.js
+++ app.js
@@ -1,39 +1,49 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js")
 
 const app = express();
 
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
 app.set("view engine", "ejs");
+
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res) {
 
-  var today = new Date();
-
-  var options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  }
-
-  var day = today.toLocaleDateString("en-US", options);
+  const day = date.getDate();
 
   res.render("list", {
-    kindOfDay: day,
+    listTitle: day,
     newListItems:items
   });
 
 });
 
 app.post("/", function(req, res) {
-  var item = req.body.newItem;
 
-  items.push(item);
+  const item = req.body.newItem;
 
-  res.redirect("/");
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+
 });
+
+app.get("/work", function(req, res) {
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+})
+
+app.get("/about", function(req, res) {
+  res.render("about")
+})
 
 app.listen(3000, function() {
   console.log("Listening on port 3000");
