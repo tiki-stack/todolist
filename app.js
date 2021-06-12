d55ffac7307a17bf7006b4c2a7e3050fc986c389
tiki-stack <tyrik.nelson1@gmail.com>
2021-06-11 21:00:33 -0400

    add ejs

--- app.js
+++ app.js
@@ -139,36 +139,3 @@ app.get("/about", function(req, res) {
 app.listen(3000, function() {
   console.log("Listening on port 3000");
 });
-
-// if (currentDay === 6 || currentDay === 0) {
-//   day = today.toString().split(" ")[0];
-//   res.render("list", {})[0];
-// } else {
-//   day = today.toString().split(" ")[0];
-// }
-
-// switch (currentDay) {
-//   case 0:
-//     day = "Sunday"
-//     break;
-//   case 1:
-//     day = "Monday"
-//     break;
-//   case 2:
-//     day = "Tuesday"
-//     break;
-//   case 3:
-//     day = "Wednesday"
-//     break;
-//   case 4:
-//     day = "Thursday"
-//     break;
-//   case 5:
-//     day = "Friday"
-//     break;
-//   case 6:
-//     day = "Saturday"
-//     break;
-//   default:
-//   console.log("Error: current day is equal to: " + currentDay);
-// }
